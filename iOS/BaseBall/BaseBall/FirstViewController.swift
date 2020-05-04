fb7eb5c737207ce11d0ea9a727a725cb341ac92d
TTOzzi <devttozzi@gmail.com>
2020-05-04 22:48:34 +0900

    [iOS] feat: 프로젝트 생성
    프로젝트 생성 및 불필요 코드 제거

---
+++ iOS/BaseBall/BaseBall/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  BaseBall
+//
+//  Created by TTOzzi on 2020/05/04.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ iOS/BaseBall/BaseBall/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BaseBall
+//
+//  Created by TTOzzi on 2020/05/04.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
