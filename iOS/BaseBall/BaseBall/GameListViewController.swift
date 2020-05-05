14e8867c35d0dcfa9e62b10547908a54cf4a8d7d
TTOzzi <devttozzi@gmail.com>
2020-05-05 16:10:54 +0900

    [iOS] feat: GameListViewController 생성
    스토리보드로 게임목록을 보여줄 ViewController 생성
    UIVIewController 를 상속받은 GameListViewController 를 만들어 게임목록을 보여줄 ViewController 의 커스텀 클래스로 설정

---
+++ iOS/BaseBall/BaseBall/GameListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameListViewController.swift
+//  BaseBall
+//
+//  Created by TTOzzi on 2020/05/05.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class GameListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
