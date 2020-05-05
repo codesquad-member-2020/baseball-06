b7a5e6a5e545c46e60dbdbe3045c1dbe697b0c00
TTOzzi <devttozzi@gmail.com>
2020-05-06 00:09:28 +0900

    [iOS] feat: 점수판 구현
    StackView 활용
    UIView 를 상속받은 CircleShapeView 커스텀 클래스를 만들어 원 모양을 가진 뷰를 만들어줌
    
    #5

---
+++ iOS/BaseBall/BaseBall/GamePlay/CircleShapeView.swift
@@ -0,0 +1,26 @@
+//
+//  CircleShapeView.swift
+//  BaseBall
+//
+//  Created by TTOzzi on 2020/05/05.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class CircleShapeView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setProperties()
+    }
+    
+    private func setProperties() {
+        layer.borderWidth = 0.5
+        layer.cornerRadius = frame.height / 2
+    }
+}
