fd02118a4858193bcb30d6cf7ba016c9047d1000
TTOzzi <devttozzi@gmail.com>
2020-05-05 19:18:05 +0900

    [iOS] feat: GameTitleView 구현
    게임 목록에 표시 될 GameTitleVIew
    xib 와 커스텀클래스를 활용해 UI 를 만들어줌

---
+++ iOS/BaseBall/BaseBall/GameTitleView.swift
@@ -0,0 +1,29 @@
+//
+//  GameTitleView.swift
+//  BaseBall
+//
+//  Created by TTOzzi on 2020/05/05.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class GameTitleView: UIView {
+    @IBOutlet weak var gameNumberLabel: UILabel!
+    @IBOutlet weak var awayTeamLabel: UILabel!
+    @IBOutlet weak var homeTeamLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setProperties()
+    }
+    
+    private func setProperties() {
+        layer.cornerRadius = frame.size.height / 5
+    }
+}
