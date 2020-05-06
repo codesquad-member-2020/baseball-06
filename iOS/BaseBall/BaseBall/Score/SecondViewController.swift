41963b79bedd2592743e08b063842ecc15413b3a
TTOzzi <devttozzi@gmail.com>
2020-05-06 17:50:52 +0900

    [iOS] feat: 팀 목록 테이블 뷰 구현
    StackView 의 equal centering 처럼 Label 의 크기에도 변하지 않는 간격을 주기 위해 Label 들의 centerX 를 기준으로 간격을 설정해줌
    프로토타입 셀을 만들어 header 에도 재사용
    UITableViewDelegate 의 메소드를 활용해 헤더 생성
    추후 모델 배열의 갯수와 IndexPath 를 비교해 하단 Total 셀 판단 예정
    label 을 폰트크기 변화 없이 bold 로 변경해주기 위해 확장해줌
    
    #6

--- iOS/BaseBall/BaseBall/Score/SecondViewController.swift
+++ iOS/BaseBall/BaseBall/Score/SecondViewController.swift
@@ -8,9 +8,20 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var scoreTableView: UITableView!
+    
+    private let scoreDataSource = ScoreDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scoreTableView.dataSource = scoreDataSource
+        scoreTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ScoreCell.identifier) as? ScoreCell else { return nil }
+        cell.backgroundColor = .opaqueSeparator
+        return cell
     }
 }
-
