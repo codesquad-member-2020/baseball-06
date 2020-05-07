334a793d3897ac23cd0f574cdafe331a29840733
Byung Hyun Choi <choi8608@gmail.com>
2020-05-07 20:27:09 +0900

    [BE] refactor: Add enum code
    - Add enum code

--- BE/src/main/java/com/codesquad/baseball06/model/type/PlayerType.java
+++ BE/src/main/java/com/codesquad/baseball06/model/type/PlayerType.java
@@ -1,14 +1,16 @@
 package com.codesquad.baseball06.model.type;
 
 public enum PlayerType {
-  PITCHER("투수"),
-  BATTER("타자");
+  PITCHER(1, "투수"),
+  BATTER(2, "타자");
 
-  PlayerType(String type) {
+  private final int code;
+  private final String type;
+
+  PlayerType(int code, String type) {
+    this.code = code;
     this.type = type;
   }
-
-  private final String type;
 
   public String getType() {
     return type;
