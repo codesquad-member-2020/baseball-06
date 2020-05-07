77c2464dd6bfd233bb2fcf9f6bf256b6facb8325
Byung Hyun Choi <choi8608@gmail.com>
2020-05-07 20:26:27 +0900

    [BE] feat: Add DB configure
    - Add schema.sql
    - Add data.sql
    
     #27

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,8 +1,7 @@
-INSERT INTO team (id, name)
-VALUES (1, 'SK w');
+INSERT INTO team (name)
+VALUES ('SK w');
 
-INSERT INTO team (id, name)
-VALUES (2, 'DOOSAN d');
+INSERT INTO team (name)
+VALUES ('DOOSAN d');
 
-INSERT INTO player (id, team_id, type, name)
-VALUES (1, 1, 1, '김투수')
+INSERT INTO player (team_id, type, name) VALUES (1, 1, '투수김');
