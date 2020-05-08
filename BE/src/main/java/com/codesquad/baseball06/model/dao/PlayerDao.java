e4585f46b3cdfa78f67d6d7a2c729951efe2f279
Byung Hyun Choi <choi8608@gmail.com>
2020-05-09 04:30:09 +0900

    [BE] refactor: Refactor PlayerDao
    - Clean code
    - Seperate query
    - Add test case
    
    #35

--- BE/src/main/java/com/codesquad/baseball06/model/dao/PlayerDao.java
+++ BE/src/main/java/com/codesquad/baseball06/model/dao/PlayerDao.java
@@ -1,14 +1,14 @@
 package com.codesquad.baseball06.model.dao;
 
-import com.codesquad.baseball06.model.dao.mapper.MultipleBatterMapper;
-import com.codesquad.baseball06.model.dao.mapper.MultiplePitcherMapper;
-import com.codesquad.baseball06.model.dao.mapper.SingleBatterMapper;
-import com.codesquad.baseball06.model.dao.mapper.SinglePitcherMapper;
+import static com.codesquad.baseball06.model.query.Player.FIND_BY_ID_AND_TYPE;
+import static com.codesquad.baseball06.model.query.Player.FIND_BY_TYPE;
+
+import com.codesquad.baseball06.model.dao.mapper.BatterMapper;
+import com.codesquad.baseball06.model.dao.mapper.PitcherMapper;
 import com.codesquad.baseball06.model.entity.Batter;
 import com.codesquad.baseball06.model.entity.Pitcher;
 import com.codesquad.baseball06.model.type.PlayerType;
 import java.util.List;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -20,55 +20,44 @@ import org.springframework.stereotype.Repository;
 public class PlayerDao {
 
   private static final Logger log = LoggerFactory.getLogger(PlayerDao.class);
-
   private final NamedParameterJdbcTemplate jdbcTemplate;
-  private final String singlePlayerQuery = new StringBuilder()
-      .append("SELECT p.id, p.team_id, p.type, p.name, p.batting_average ")
-      .append("FROM player p ")
-      .append("WHERE p.id = :id ")
-      .append("  AND p.type = :type")
-      .toString();
-  private final String multiplePlayerQuery = new StringBuilder()
-      .append("SELECT p.id, p.team_id, p.type, p.name, p.batting_average ")
-      .append("FROM player p ")
-      .append("WHERE p.type = :type")
-      .toString();
+  private final PitcherMapper pitcherMapper;
+  private final BatterMapper batterMapper;
 
-  public PlayerDao(NamedParameterJdbcTemplate jdbcTemplate) {
+  public PlayerDao(NamedParameterJdbcTemplate jdbcTemplate, PitcherMapper pitcherMapper,
+      BatterMapper batterMapper) {
     this.jdbcTemplate = jdbcTemplate;
+    this.pitcherMapper = pitcherMapper;
+    this.batterMapper = batterMapper;
   }
 
-  public Optional<Pitcher> findPitcherById(Long id) {
+  public Pitcher findPitcherById(Long id) {
     SqlParameterSource namedParameters = new MapSqlParameterSource()
         .addValue("id", id)
         .addValue("type", PlayerType.PITCHER.getCode());
 
-    return Optional.ofNullable(
-        jdbcTemplate.queryForObject(singlePlayerQuery, namedParameters, new SinglePitcherMapper()));
+    return jdbcTemplate.queryForObject(FIND_BY_ID_AND_TYPE, namedParameters, pitcherMapper).get(0);
   }
 
   public List<Pitcher> findPitchers() {
     SqlParameterSource namedParameters = new MapSqlParameterSource()
         .addValue("type", PlayerType.PITCHER.getCode());
 
-    return jdbcTemplate
-        .queryForObject(multiplePlayerQuery, namedParameters, new MultiplePitcherMapper());
+    return jdbcTemplate.queryForObject(FIND_BY_TYPE, namedParameters, pitcherMapper);
   }
 
-  public Optional<Batter> findBatterById(Long id) {
+  public Batter findBatterById(Long id) {
     SqlParameterSource namedParameters = new MapSqlParameterSource()
         .addValue("id", id)
         .addValue("type", PlayerType.BATTER.getCode());
 
-    return Optional.ofNullable(
-        jdbcTemplate.queryForObject(singlePlayerQuery, namedParameters, new SingleBatterMapper()));
+    return jdbcTemplate.queryForObject(FIND_BY_ID_AND_TYPE, namedParameters, batterMapper).get(0);
   }
 
   public List<Batter> findBatters() {
     SqlParameterSource namedParameters = new MapSqlParameterSource()
         .addValue("type", PlayerType.BATTER.getCode());
 
-    return jdbcTemplate.queryForObject(
-        multiplePlayerQuery, namedParameters, new MultipleBatterMapper());
+    return jdbcTemplate.queryForObject(FIND_BY_TYPE, namedParameters, batterMapper);
   }
 }
--- BE/src/test/java/com/codesquad/baseball06/model/dao/PlayerDaoTest.java
+++ BE/src/test/java/com/codesquad/baseball06/model/dao/PlayerDaoTest.java
@@ -1,14 +1,17 @@
 package com.codesquad.baseball06.model.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import com.codesquad.baseball06.model.entity.Batter;
 import com.codesquad.baseball06.model.entity.Pitcher;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 @SpringBootTest
 class PlayerDaoTest {
@@ -20,14 +23,20 @@ class PlayerDaoTest {
 
   @Test
   void findPlayerById_pitcher() {
-    assertThat(playerDao.findPitcherById(1L).get())
+    assertThat(playerDao.findPitcherById(1L))
         .isNotNull()
         .isInstanceOf(Pitcher.class);
   }
 
   @Test
+  void findPlayerById_null() {
+    assertThatExceptionOfType(EmptyResultDataAccessException.class)
+        .isThrownBy(() -> playerDao.findPitcherById(999L));
+  }
+
+  @Test
   void findPlayerById_batter() {
-    assertThat(playerDao.findBatterById(3L).get())
+    assertThat(playerDao.findBatterById(3L))
         .isNotNull()
         .isInstanceOf(Batter.class);
   }
@@ -36,13 +45,15 @@ class PlayerDaoTest {
   void findPitchers() {
     assertThat(playerDao.findPitchers())
         .isNotNull()
-        .isInstanceOf(Pitcher.class);
+        .isInstanceOf(ArrayList.class)
+        .hasSizeGreaterThan(1);
   }
 
-//  @Test
-//  void findBatters() {
-//    assertThat(playerDao.findBatters())
-//        .isNotNull()
-//        .isInstanceOf(Batter.class);
-//  }
+  @Test
+  void findBatters() {
+    assertThat(playerDao.findBatters())
+        .isNotNull()
+        .isInstanceOf(ArrayList.class)
+        .hasSizeGreaterThan(1);
+  }
 }
