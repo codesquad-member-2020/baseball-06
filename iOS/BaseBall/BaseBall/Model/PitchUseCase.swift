3db2e6051e05b9b8f76ca43f659cfc059442532c
TTOzzi <devttozzi@gmail.com>
2020-05-08 15:39:39 +0900

    [iOS] feat: 서버에서 받아 온 데이터를 PitchResult 객체로 만들어 줄 usecase 구현
    여기서도 Result 타입을 활용함
    Networkmanager 에서 발생한 오류가 있다면 그걸 그대로 넘겨줌
    
    #32

---
+++ iOS/BaseBall/BaseBall/Model/PitchUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  PitchUseCase.swift
+//  BaseBall
+//
+//  Created by TTOzzi on 2020/05/08.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+struct PitchUseCase {
+    static func pitch(with manager: NetworkManager, completion: @escaping (Result<PitchResult, NetworkError>) -> ()) {
+        manager.getResource(from: NetworkManager.EndPoints.Pitch, handler: { result in
+            switch result {
+            case .success(let data):
+                guard let response = try? JSONDecoder().decode(PitchResponse.self, from: data) else {
+                    completion(.failure(.DecodingError))
+                    return
+                }
+                completion(.success(response.body))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+}
