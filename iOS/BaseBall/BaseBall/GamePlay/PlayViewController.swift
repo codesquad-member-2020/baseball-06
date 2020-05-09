5fcc8321521da93abac8afc82b0ede04208dce7d
TTOzzi <devttozzi@gmail.com>
2020-05-09 23:14:10 +0900

    [iOS] feat: API 통신으로 받아온 데이터로 뷰 업데이트
    Pitch 버튼을 누르면 네트워크에 요청을 보내고 데이터를 받아옴
    받아온 데이터로 뷰 업데이트
    
    받아온 데이터를 저장 할 모델을 만들어 데이터 업데이트와 뷰 업데이트 로직 분리 필요
    에러처리 필요
    
    #32

--- iOS/BaseBall/BaseBall/GamePlay/PlayViewController.swift
+++ iOS/BaseBall/BaseBall/GamePlay/PlayViewController.swift
@@ -13,6 +13,7 @@ class PlayViewController: UIViewController {
     
     @IBOutlet weak var playerInfoTableView: PlayerInfoTableView!
     @IBOutlet weak var pitchingResultTableView: UITableView!
+    @IBOutlet weak var scoreStackView: OverallScoreView!
     
     private let playerInfoDataSource = PlayerInfoDataSource()
     private let pitchingResultDataSource = PitchingResultDataSource()
@@ -21,6 +22,20 @@ class PlayViewController: UIViewController {
         super.viewDidLoad()
         playerInfoTableView.dataSource = playerInfoDataSource
         pitchingResultTableView.dataSource = pitchingResultDataSource
+    }
+    
+    @IBAction func pitchButtonTapped(_ sender: Any) {
+        PitchUseCase.pitch(with: NetworkManager()) { result in
+            switch result {
+            case .success(let pitchResult):
+                let result = pitchResult.inningStatus.components(separatedBy: " ").map { Int(String($0.first!))! }
+                DispatchQueue.main.async {
+                    self.scoreStackView.updateScore(strike: result[0], ball: result[1], out: result[2])
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
