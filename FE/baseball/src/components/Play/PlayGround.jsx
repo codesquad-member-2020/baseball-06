4bae523d1a932dca80c77a30e670772bf615b3aa
Elllin <noto01@naver.com>
2020-05-10 21:46:17 +0900

    [fe] feat: hit 할때 rotate추가

--- FE/baseball/src/components/Play/PlayGround.jsx
+++ FE/baseball/src/components/Play/PlayGround.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect } from "react";
-import styled, { ThemeProvider, css } from "styled-components";
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import styled, { ThemeProvider, css, keyframes } from "styled-components";
 import theme from "../../styles/theme";
 import ground from "../../styles/images/ground.jpg";
 
@@ -12,24 +12,27 @@ const img = "http://ih2.redbubble.net/image.12303484.4729/sticker,375x360.png";
 // "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bff79450-9f9c-4baf-af12-b6289ab026d8/d5xpfor-f6658e45-58a4-44e4-b204-431b39285355.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYmZmNzk0NTAtOWY5Yy00YmFmLWFmMTItYjYyODlhYjAyNmQ4XC9kNXhwZm9yLWY2NjU4ZTQ1LTU4YTQtNDRlNC1iMjA0LTQzMWIzOTI4NTM1NS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.U3JxM9boQ07PxcmUJEqYcM8zqJ_u1TmDaZ_yC8Nku-0";
 
 function PlayGround() {
-  const playerRef = useRef();
-  const [coord, setCoord] = useState(0);
-  // const [count, setCount] = useState(0);
+  const [batterCoord, setBatterCoord] = useState(0);
   const [rotation, setRotation] = useState(187);
   const [top, setTop] = useState(65);
   const [left, setLeft] = useState(43);
   const [deg, setDeg] = useState(-28);
+
   const [ballTopCoord, setBallTopCoord] = useState(45);
   const [ballLeftCoord, setBallLeftCoord] = useState(55);
   const [hitterDisplay, setHitterDisplay] = useState(true);
   const [resultDisplay, setResultDisplay] = useState(true);
   const [pitchBtnDisplay, setPitchBtnDisplay] = useState("block");
   const [result, setResult] = useState("");
-  const x = useRef(0);
+  const [hitCoord, setHitCoord] = useState(0);
+  const batterCoordCount = useRef(0);
   const ballCoord = useRef(0);
+  const hitCoordCount = useRef(0);
   const count = useRef(0);
-  const rafId = useRef();
-  const rafBall = useRef(0);
+  const batter = useRef();
+  const hitIncreaseRaf = useRef();
+  const hitDecreaseRaf = useRef();
+  // const ballRaf = useRef(0);
   const onPitch = () => {
     // animation();
     pitchAnimation();
@@ -38,13 +41,21 @@ function PlayGround() {
   };
 
   const pitchAnimation = () => {
+    let ballRaf = null;
+    const ballEndCoord = 150;
     setPitchBtnDisplay("none");
     setResultDisplay("block");
     setResult("기다려");
-    if (ballCoord.current > 270) {
+
+    if (ballCoord.current > 100) {
+      batter.current.style.transform = "rotateY(140deg)";
+      setTimeout(() => (batter.current.style.transform = "rotateY(0deg)"), 100);
+    }
+    if (ballCoord.current > ballEndCoord) {
       ballCoord.current = 0;
       setBallTopCoord(45);
       setBallLeftCoord(55);
+      cancelAnimationFrame(ballRaf);
       fetch("http://15.164.101.161:8080/dev/dotest")
         .then((res) => res.json())
         .then((data) => {
@@ -53,33 +64,35 @@ function PlayGround() {
             setPitchBtnDisplay("block");
             setResultDisplay("none");
           }, 1000);
-
-          console.log(data.body.battingResult);
           if (data.body.battingResult === "HIT") {
             count.current++;
             setHitterDisplay(false);
             setTimeout(() => setHitterDisplay(true), 1000);
-            animation();
+            moveBatter();
           }
         });
 
       return;
     }
     ballCoord.current += 5;
-    setBallTopCoord((prevState) => prevState + 3);
-    setBallLeftCoord((prevState) => prevState - 1);
-    rafBall.current = requestAnimationFrame(pitchAnimation);
+    setBallTopCoord((prevState) => prevState + 5);
+    setBallLeftCoord((prevState) => prevState - 2.5);
+    ballRaf = requestAnimationFrame(pitchAnimation);
   };
 
-  const animation = () => {
-    if (x.current > 385) {
-      x.current = 0;
-      return cancelAnimationFrame(rafId.current);
-    }
-    setCoord(x.current);
+  const moveBatter = () => {
+    const movingSpeed = 13;
+    const endCoord = 385;
+    let batterRaf = null;
 
-    x.current += 15;
-    rafId.current = requestAnimationFrame(animation);
+    if (batterCoordCount.current > endCoord) {
+      batterCoordCount.current = 0;
+      return cancelAnimationFrame(batterRaf);
+    }
+    setBatterCoord(batterCoordCount.current);
+
+    batterCoordCount.current += movingSpeed;
+    batterRaf = requestAnimationFrame(moveBatter);
   };
 
   return (
@@ -92,23 +105,25 @@ function PlayGround() {
         </PitcherArea>
         <Ground top={top} left={left} deg={deg}>
           <Player
+            ref={batter}
+            hitCoord={hitCoord}
             rotation={rotation}
-            coord={coord}
+            coord={batterCoord}
             count={count}
             hitterDisplay={hitterDisplay}
           />
         </Ground>
         <Ground1 top={top} left={left} deg={deg}>
-          <Player1 rotation={rotation} coord={coord} count={count} />
+          <Player1 rotation={rotation} coord={batterCoord} count={count} />
         </Ground1>
         <Ground2>
-          <Player2 rotation={rotation} coord={coord} count={count} />
+          <Player2 rotation={rotation} coord={batterCoord} count={count} />
         </Ground2>
         <Ground3>
-          <Player3 rotation={rotation} coord={coord} count={count} />
+          <Player3 rotation={rotation} coord={batterCoord} count={count} />
         </Ground3>
         <Ground4>
-          <Player4 rotation={rotation} coord={coord} count={count} />
+          <Player4 rotation={rotation} coord={batterCoord} count={count} />
         </Ground4>
         <PitchBtnBox display={pitchBtnDisplay} onClick={onPitch}>
           Pitch
@@ -117,6 +132,18 @@ function PlayGround() {
     </ThemeProvider>
   );
 }
+
+const batterHit = keyframes`
+  0% {
+    transform: rotateY(0deg);
+  }
+  50% {
+    transform: rotateY(180deg);
+  }
+  100% {
+    transform: rotateY(0deg);
+  }
+  `;
 
 const box = css`
   margin-top: 20px;
@@ -231,6 +258,9 @@ display: ${(props) => {
   width: 80px;
   height: 100px;
 
+  transform: ${(props) => `rotateY(${props.hitCoord}deg)`};
+ 
+
   background: no-repeat url(${img});
   background-position: center;
   background-size: cover;
@@ -299,14 +329,6 @@ const PitchBtnBox = styled.button`
     #e75516
   );
   box-shadow: 0 4px 15px 0 rgba(252, 104, 110, 0.75);
-  /* background-image: linear-gradient(
-    to right,
-    #25aae1,
-    #40e495,
-    #30dd8a,
-    #2bb673
-  );
-  box-shadow: 0 4px 15px 0 rgba(49, 196, 190, 0.75); */
   &:hover {
     background-position: 100% 0;
     transition: all 0.4s ease-in-out;
---
+++ FE/baseball/src/constants/url.js
@@ -0,0 +1,2 @@
+const GAME_PLAY_URL = `http://15.164.101.161:8080/dev/dotest`;
+export { MAIN_URL };
