1b958e0258a6695a71b0ae8706516c9de463337a
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-10 17:50:02 +0900

    [BE] refactor: scoreMapper to return scoreMap
    - change structure from using list to Map
    
    #37

--- BE/src/main/java/com/codesquad/baseball06/model/dao/mapper/ScoreMapper.java
+++ BE/src/main/java/com/codesquad/baseball06/model/dao/mapper/ScoreMapper.java
@@ -4,23 +4,29 @@ import com.codesquad.baseball06.dto.ScoreDto;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ScoreMapper implements RowMapper<List<Integer>> {
+public class ScoreMapper implements RowMapper<Map<String, Integer>> {
 
   @Override
-  public List<Integer> mapRow(ResultSet rs, int rowNum) throws SQLException {
+  public Map<String, Integer> mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-    List<Integer> scoreList = new ArrayList<>();
+    Map<String, Integer> scoreMap = new HashMap<>();
 
     do {
-      int nowScore = rs.getInt("score");
-      scoreList.add(nowScore);
+      if (rs.getLong("id") == 1) {
+        scoreMap.put("away", rs.getInt("score"));
+      }
+      if (rs.getLong("id") == 2) {
+        scoreMap.put("home", rs.getInt("score"));
+      }
     } while (rs.next());
 
-    return scoreList;
+    return scoreMap;
   }
 }
