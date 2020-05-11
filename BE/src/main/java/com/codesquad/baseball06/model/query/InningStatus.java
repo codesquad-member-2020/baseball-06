01c2c1ec6c7de05a3faf910ed01685a6f68fa202
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-11 23:41:50 +0900

    [BE] refactor: delete files
    - delete duplicate files due to checkout files from dto branch

--- BE/src/main/java/com/codesquad/baseball06/model/query/InningStatus.java
+++
@@ -1,7 +0,0 @@
-package com.codesquad.baseball06.model.query;
-
-public class InningStatus {
-
-  public static final String FIND_BY_ID = "SELECT i.id, i.half_inning_id, i.strike_count, i.ball_count, i.out_count FROM inning_status i WHERE i.id = :id";
-  public static final String INSERT = "INSERT INTO inning_status (half_inning_id) VALUES (:half_inning_id)";
-}
