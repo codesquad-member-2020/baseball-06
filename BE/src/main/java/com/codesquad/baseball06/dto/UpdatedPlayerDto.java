bfa65d65e2d76a3c7e34f111d1e20b705a8bed67
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-11 11:14:20 +0900

    [BE] feat: add batterIndex on UpdatedPlayerDto
    - UpdatedPlayerDto to have batterIndex method
    - split createUpedatedBatterDto and PitcherDto
    
    #37

--- BE/src/main/java/com/codesquad/baseball06/dto/UpdatedPlayerDto.java
+++ BE/src/main/java/com/codesquad/baseball06/dto/UpdatedPlayerDto.java
@@ -1,17 +1,42 @@
 package com.codesquad.baseball06.dto;
 
+import com.codesquad.baseball06.model.entity.Batter;
+import com.codesquad.baseball06.model.entity.Pitcher;
 import com.codesquad.baseball06.model.type.PlayerType;
 
 public class UpdatedPlayerDto {
+
   private Long id;
   private String name;
   private PlayerType type;
-  private String teamName;
+  private Long teamId;
+  private Double battingAverage;
+  private Integer batterIndex;
 
-  public UpdatedPlayerDto(Long id, String name, PlayerType type, String teamName) {
+  private UpdatedPlayerDto(Long id, String name, PlayerType type, Long teamId) {
     this.id = id;
     this.name = name;
     this.type = type;
-    this.teamName = teamName;
+    this.teamId = teamId;
+  }
+
+  private UpdatedPlayerDto(Long id, String name, PlayerType type, Long teamId,
+      Double battingAverage, Integer batterIndex) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.teamId = teamId;
+    this.battingAverage = battingAverage;
+    this.batterIndex = batterIndex;
+  }
+
+  public static UpdatedPlayerDto createUpdatedBatterDto(Batter batter) {
+    return new UpdatedPlayerDto(batter.getId(), batter.getName(), PlayerType.BATTER,
+        batter.getTeamId(), batter.getBattingAverage(), batter.getBatterIndex());
+  }
+
+  public static UpdatedPlayerDto createUpdatedPitcherDto(Pitcher pitcher) {
+    return new UpdatedPlayerDto(pitcher.getId(), pitcher.getName(), PlayerType.PITCHER,
+        pitcher.getTeamId());
   }
 }
