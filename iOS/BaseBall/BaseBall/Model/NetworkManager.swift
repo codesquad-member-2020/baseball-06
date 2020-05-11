f16dd0e9abdbd745138c4795642a92ede0033771
TTOzzi <devttozzi@gmail.com>
2020-05-11 17:02:01 +0900

    [iOS] feat: API 통신 성공 임시데이터 생성
    테스트용 임시 데이터 생성
    
    #32

--- iOS/BaseBall/BaseBall/Model/NetworkManager.swift
+++ iOS/BaseBall/BaseBall/Model/NetworkManager.swift
@@ -38,6 +38,36 @@ struct NetworkManager: NetworkManageable {
     }
 }
 
+struct MockNetworkSuccessStub: NetworkManageable {
+    func getResource(from: String, handler: @escaping ((Result<Data, NetworkError>) -> Void)) {
+        let result = PitchResponse.init(body: PitchResult.init(inningStatus: InningStatus.init(inningNum: 1,
+                                                                                               inningType: "초",
+                                                                                               strike: 1,
+                                                                                               ball: 1,
+                                                                                               out: 1),
+                                                               updatedBaseman: Base.init(firstBase: BaseInfo.init(name: "a",
+                                                                                                                  id: 1),
+                                                                                         secondBase: BaseInfo.init(name: nil,
+                                                                                                                   id: nil),
+                                                                                         thirdBase: BaseInfo.init(name: nil,
+                                                                                                                  id: nil)),
+                                                               updatedScore: Score.init(home: 1, away: 2),
+                                                               updatedPlayer: [Player.init(id: 1,
+                                                                                           name: "투수임",
+                                                                                           type: "투수",
+                                                                                           teamType: "Home",
+                                                                                           teamName: "A팀"),
+                                                                               Player.init(id: 2,
+                                                                                           name: "타자임",
+                                                                                           type: "타자",
+                                                                                           teamType: "Away",
+                                                                                           teamName: "B팀")]))
+        let encoder = JSONEncoder()
+        guard let data = try? encoder.encode(result) else { return }
+        handler(.success(data))
+    }
+}
+
 struct MockNetworkFailureStub: NetworkManageable {
     func getResource(from: String, handler: @escaping ((Result<Data, NetworkError>) -> Void)) {
         handler(.failure(.DataNotFound))
