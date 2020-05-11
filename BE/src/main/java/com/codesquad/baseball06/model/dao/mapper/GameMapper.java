86baa8e922e7dfdd75d3ce960656ff0be62204d2
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-11 23:37:31 +0900

    [BE] refactor: change convention
    - keep coding convention
    
    #37

--- BE/src/main/java/com/codesquad/baseball06/model/dao/mapper/GameMapper.java
+++ BE/src/main/java/com/codesquad/baseball06/model/dao/mapper/GameMapper.java
@@ -25,13 +25,14 @@ public class GameMapper implements RowMapper<List<Game>> {
     do {
       Long awayId = rs.getLong("away_id");
       Long homeId = rs.getLong("home_id");
-      gameList.add(Game.create(rs.getLong("id")
-          , teamDao.findTeamById(awayId)
-          , teamDao.findTeamById(homeId)
-          , rs.getString("away_user")
-          , rs.getString("home_user")
-          , rs.getBoolean("end")
-          , rs.getTimestamp("created_at")));
+      gameList.add(Game.create(
+          rs.getLong("id"),
+          teamDao.findTeamById(awayId),
+          teamDao.findTeamById(homeId),
+          rs.getString("away_user"),
+          rs.getString("home_user"),
+          rs.getBoolean("end"),
+          rs.getTimestamp("created_at")));
     } while (rs.next());
 
     return gameList;
