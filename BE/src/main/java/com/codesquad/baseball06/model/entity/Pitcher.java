2824f50ff431ef252066570eac10e529c6531c03
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-11 11:13:22 +0900

    [BE] add batterIndex for entities that extend Player
    - TODO: refactor to COMPLETELY split the fields between Batter and Pitcher
    
    #37

--- BE/src/main/java/com/codesquad/baseball06/model/entity/Pitcher.java
+++ BE/src/main/java/com/codesquad/baseball06/model/entity/Pitcher.java
@@ -20,9 +20,17 @@ public class Pitcher extends Player {
     return new Pitcher(teamId, name);
   }
 
+  //TODO 조금 더 상속에 대한 개념을 어떻게 적극적으로 사용할 것인가를 질문해보고 싶습니다.. 어려워.
+
   @Override
   @JsonInclude(JsonInclude.Include.NON_NULL)
   public Double getBattingAverage() {
+    return null;
+  }
+
+  @Override
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  public Integer getBatterIndex() {
     return null;
   }
 }
