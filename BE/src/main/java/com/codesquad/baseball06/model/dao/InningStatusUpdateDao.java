8484ddf892e85cdb57570634bc77bd10f16a21bd
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-13 03:06:24 +0900

    [BE] feat: add updateBaseStatusResult on InningStatusUpdateDao
    - get HalfInning entity and update BaseStatus
    - obtain current base players from having injection from InningStatusDtoReturnDao

--- BE/src/main/java/com/codesquad/baseball06/model/dao/InningStatusUpdateDao.java
+++ BE/src/main/java/com/codesquad/baseball06/model/dao/InningStatusUpdateDao.java
@@ -1,8 +1,12 @@
 package com.codesquad.baseball06.model.dao;
 
+import com.codesquad.baseball06.dto.UpdatedBasemanDto;
+import com.codesquad.baseball06.dto.UpdatedPlayerDto;
 import com.codesquad.baseball06.model.entity.HalfInning;
 import com.codesquad.baseball06.model.query.UpdateInningStatusQuery;
 import com.codesquad.baseball06.model.type.BattingResult;
+import com.codesquad.baseball06.model.type.PlayerType;
+import java.util.List;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -12,9 +16,12 @@ import org.springframework.stereotype.Repository;
 public class InningStatusUpdateDao {
 
   private final NamedParameterJdbcTemplate jdbcTemplate;
+  private final InningStatusDtoReturnDao inningStatusDtoReturnDao;
 
-  public InningStatusUpdateDao(NamedParameterJdbcTemplate jdbcTemplate) {
+  public InningStatusUpdateDao(NamedParameterJdbcTemplate jdbcTemplate,
+      InningStatusDtoReturnDao inningStatusDtoReturnDao) {
     this.jdbcTemplate = jdbcTemplate;
+    this.inningStatusDtoReturnDao = inningStatusDtoReturnDao;
   }
 
   public int createNewInningStatus(HalfInning halfInning) {
@@ -36,11 +43,11 @@ public class InningStatusUpdateDao {
     }
     if (battingResult == BattingResult.HIT) {
       jdbcTemplate.update(UpdateInningStatusQuery.INITIALIZE_ALL_COUNT, sqlParameterSource);
-      return updateBaseStatusResult(battingResult);
+      return updateBaseStatusResult(halfInning);
     }
     if (battingResult == BattingResult.BASE_ON_BALL) {
       jdbcTemplate.update(UpdateInningStatusQuery.INITIALIZE_ALL_COUNT, sqlParameterSource);
-      return updateBaseStatusResult(battingResult);
+      return updateBaseStatusResult(halfInning);
     }
     if (battingResult == BattingResult.OUT) {
       return jdbcTemplate.update(UpdateInningStatusQuery.INCREASE_OUT_COUNT_AND_INITIALIZE_OTHERS,
@@ -54,8 +61,23 @@ public class InningStatusUpdateDao {
     throw new RuntimeException("무엇인가가 잘못되었습니다.");
   }
 
-  public int updateBaseStatusResult(BattingResult battingResult) {
-    
-    //insert your code here
+  public int updateBaseStatusResult(HalfInning halfInning) {
+    List<UpdatedPlayerDto> getPreviousPlayers = inningStatusDtoReturnDao
+        .getUpdatedPlayers(halfInning.getGameId());
+    UpdatedBasemanDto getPreviousBaseman = inningStatusDtoReturnDao.getUpdatedBaseman();
+    SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+        .addValue("half_inning_id", halfInning.getId())
+        .addValue("first_base", getPreviousPlayers.get(PlayerType.BATTER.getCode()))
+        .addValue("second_base", getPreviousBaseman.getFirstBase().getId())
+        .addValue("third_base", getPreviousBaseman.getSecondBase().getId());
+
+    return jdbcTemplate.update(UpdateInningStatusQuery.UPDATE_INNING_PLAYER, sqlParameterSource);
+  }
+
+  public int createNewBaseStatus(Long halfInningId) {
+    SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+        .addValue("half_inning_id", halfInningId);
+
+    return jdbcTemplate.update(UpdateInningStatusQuery.CREATE_NEW_BASE_STATUS, sqlParameterSource);
   }
 }
