0f3e31640bff3836471d44d1479c6e79d0dfea4b
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-12 22:40:59 +0900

    [BE] feat: create UpdateInningStatusQuery
    - add query collection of increasing strike count, ball count, out count and initialize all count, create new inning status

---
+++ BE/src/main/java/com/codesquad/baseball06/model/query/UpdateInningStatusQuery.java
@@ -0,0 +1,23 @@
+package com.codesquad.baseball06.model.query;
+
+public class UpdateInningStatusQuery {
+
+  public static final String INCREASE_STRIKE_COUNT =
+      "UPDATE inning_status SET strike_count = strike_count + 1 "
+          + "WHERE half_inning_id = :half_inning_id;";
+  public static final String INCREASE_BALL_COUNT =
+      "UPDATE inning_status SET ball_count = ball_count + 1 "
+          + "WHERE half_inning_id = :half_inning_id;";
+  public static final String INITIALIZE_ALL_COUNT =
+      "UPDATE inning_status SET strike_count = 0, ball_count = 0, out_count = 0 "
+          + "WHERE half_inning_id = :half_inning_id;";
+  public static final String INCREASE_OUT_COUNT_AND_INITIALIZE_OTHERS =
+      "UPDATE inning_status \n"
+          + "SET strike_count = 0,\n"
+          + "ball_count = 0,\n"
+          + "out_count = out_count + 1\n"
+          + "WHERE half_inning_id = :half_inning_id;";
+  public static final String CREATE_NEW_INNING_STATUS =
+      "INSERT INTO inning_status (strike_count, ball_count, out_count, half_inning_id)"
+          + "VALUES (0, 0, 0, :half_inning_id);";
+}
