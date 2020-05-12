a0465499f09cc869a5b278b0bfa9a5aca8e2909d
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-12 22:53:53 +0900

    [BE] fix: addHalfInning to return DB inserted halfinning
    - the halfinning will return database's auto-incremented id

--- BE/src/main/java/com/codesquad/baseball06/service/InningService.java
+++ BE/src/main/java/com/codesquad/baseball06/service/InningService.java
@@ -2,6 +2,7 @@ package com.codesquad.baseball06.service;
 
 
 import com.codesquad.baseball06.model.dao.HalfInningDao;
+import com.codesquad.baseball06.model.dao.InningStatusDao;
 import com.codesquad.baseball06.model.entity.Batter;
 import com.codesquad.baseball06.model.entity.HalfInning;
 import com.codesquad.baseball06.model.entity.Pitcher;
@@ -16,17 +17,26 @@ public class InningService {
 
   private static final Logger log = LoggerFactory.getLogger(InningService.class);
   private final HalfInningDao halfInningDao;
+  private final InningStatusDao inningStatusDao;
   private final PlateAppearanceService paService;
 
   public InningService(HalfInningDao halfInningDao,
+      InningStatusDao inningStatusDao,
       PlateAppearanceService paService) {
     this.halfInningDao = halfInningDao;
+    this.inningStatusDao = inningStatusDao;
     this.paService = paService;
   }
 
-  public int addHalfInning(HalfInning addedHalfInning) {
-    return halfInningDao.create(
-        addedHalfInning.getGameId(), addedHalfInning.getIndex(), addedHalfInning.getType());
+  public void addHalfInning(HalfInning addedButBeforeInsertedDBHalfInning) {
+    halfInningDao.create(
+        addedButBeforeInsertedDBHalfInning.getGameId(),
+        addedButBeforeInsertedDBHalfInning.getIndex(),
+        addedButBeforeInsertedDBHalfInning.getType());
+
+    HalfInning afterDBInsertedHalfInning = getHalfInnning(
+        addedButBeforeInsertedDBHalfInning.getGameId());
+    inningStatusDao.createNewInningStatus(afterDBInsertedHalfInning);
   }
 
   public HalfInning getHalfInnning(Long gameId) {
@@ -38,7 +48,7 @@ public class InningService {
   }
 
   public BattingResult proceedPA(HalfInning halfInning) {
-    Batter batter = Batter.create(1L, "Sigrid", 0.333);
+    Batter batter = Batter.create(1L, "Sigrid", 0.222);
     Pitcher pitcher = Pitcher.create(2L, "Dan");
     BattingResult battingResult = paService.doPitching(pitcher, batter);
     return paService.postPitching(halfInning, battingResult);
