ee5c415151196cca187c444308b2731bfc6418e0
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-12 22:51:32 +0900

    [BE] refactor: split logic for adding new halfinning
    - split to seperate method that returns true or false

--- BE/src/main/java/com/codesquad/baseball06/service/GameService.java
+++ BE/src/main/java/com/codesquad/baseball06/service/GameService.java
@@ -8,6 +8,7 @@ import com.codesquad.baseball06.model.type.BattingResult;
 import com.codesquad.baseball06.model.type.InningType;
 import com.codesquad.baseball06.model.type.TeamType;
 import com.google.common.collect.Iterables;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -33,6 +34,11 @@ public class GameService {
         HalfInning addedHalfInning = game.addHalfInning();
         inningService.addHalfInning(addedHalfInning);
 
+        ArrayList<HalfInning> listAfterIdSetOfAddedHalfInning = new ArrayList<>();
+        listAfterIdSetOfAddedHalfInning
+            .add(inningService.getHalfInnning(addedHalfInning.getGameId()));
+        game.setEarlyInningList(listAfterIdSetOfAddedHalfInning);
+
         return inningService.getHalfInnning(addedHalfInning.getGameId());
       }
       throw new RuntimeException("이미 게임이 시작되었습니다.");
@@ -42,32 +48,41 @@ public class GameService {
   }
 
   public BattingResult proceed(Game game) {
-    if (!game.isNewGame() && !game.isEndGame()) {
-      if (Iterables.getLast(game.getEarlyInningList()).getEnd() &&
-          Iterables.getLast(game.getLateInningList()).getEnd()) {
-        if (game.getEarlyInningList().size() == game.getLateInningList().size()) {
-          inningService.addHalfInning(HalfInning.create(
-              game.getId(),
-              game.getEarlyInningList().size() + 1,
-              InningType.EARLY)
-          );
-        } else {
-          inningService.addHalfInning(HalfInning.create(
-              game.getId(),
-              game.getLateInningList().size() + 1,
-              InningType.LATE)
-          );
-        }
-      }
 
+    if (!game.isNewGame() && !game.isEndGame()) {
+      checkAddNewHalfInningBeforeProceed(game);
       if (!Iterables.getLast(game.getEarlyInningList()).getEnd()) {
         return inningService.proceedPA(Iterables.getLast(game.getEarlyInningList()));
-      } else if (!Iterables.getLast(game.getLateInningList()).getEnd()) {
+      }
+
+      if (!Iterables.getLast(game.getLateInningList()).getEnd()) {
         return inningService.proceedPA(Iterables.getLast(game.getLateInningList()));
       }
     }
-
     throw new RuntimeException("투구를 진행할 수 없는 게임입니다.");
+  }
+
+  public void checkAddNewHalfInningBeforeProceed(Game game) {
+    if (Iterables.getLast(game.getEarlyInningList()).getEnd() &&
+        Iterables.getLast(game.getLateInningList()).getEnd()) {
+
+      if (game.getEarlyInningList().size() == game.getLateInningList().size()) {
+        inningService.addHalfInning(HalfInning.create(
+            game.getId(),
+            game.getEarlyInningList().size() + 1,
+            InningType.EARLY)
+        );
+      }
+      ;
+
+      if (game.getEarlyInningList().size() != game.getLateInningList().size()) {
+        inningService.addHalfInning(HalfInning.create(
+            game.getId(),
+            game.getLateInningList().size() + 1,
+            InningType.LATE)
+        );
+      }
+    }
   }
 
   public Game getGame(Long gameId) {
