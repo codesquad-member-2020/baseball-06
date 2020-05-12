904fb918db4b617c69b43c314eb61a9caf71f044
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-12 17:54:06 +0900

    [BE] feat: add getHalfInningList and proceedPA logic in InningService
    - proceedPA logic to return battingResult after executing paService's doPitching
    - In this commit, batter and pitcher is MOCK data (Sigrid, Dan. NO DB FETCHED)
    - getHalfInningList logic to find All halfInnings from DB by using gameId
    - PlateApperanceService to return BattingResult, between Strike OR ball
    
    #37

--- BE/src/main/java/com/codesquad/baseball06/service/InningService.java
+++ BE/src/main/java/com/codesquad/baseball06/service/InningService.java
@@ -2,7 +2,11 @@ package com.codesquad.baseball06.service;
 
 
 import com.codesquad.baseball06.model.dao.HalfInningDao;
+import com.codesquad.baseball06.model.entity.Batter;
 import com.codesquad.baseball06.model.entity.HalfInning;
+import com.codesquad.baseball06.model.entity.Pitcher;
+import com.codesquad.baseball06.model.type.BattingResult;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,9 +16,12 @@ public class InningService {
 
   private static final Logger log = LoggerFactory.getLogger(InningService.class);
   private final HalfInningDao halfInningDao;
+  private final PlateAppearanceService paService;
 
-  public InningService(HalfInningDao halfInningDao) {
+  public InningService(HalfInningDao halfInningDao,
+      PlateAppearanceService paService) {
     this.halfInningDao = halfInningDao;
+    this.paService = paService;
   }
 
   public int addHalfInning(HalfInning addedHalfInning) {
@@ -24,5 +31,16 @@ public class InningService {
 
   public HalfInning getHalfInnning(Long gameId) {
     return halfInningDao.findHalfInningByGameIdAndLast(gameId);
+  }
+
+  public List<HalfInning> getHalfInnningList(Long gameId) {
+    return halfInningDao.findHalfInningByGameId(gameId);
+  }
+
+  public BattingResult proceedPA(HalfInning halfInning) {
+    Batter batter = Batter.create(1L, "Sigrid", 0.333);
+    Pitcher pitcher = Pitcher.create(2L, "Dan");
+    BattingResult battingResult = paService.doPitching(pitcher, batter);
+    return paService.postPitching(halfInning, battingResult);
   }
 }
