71a540c61affbbfa462a0829af653dc1b042a0b1
Byung Hyun Choi <choi8608@gmail.com>
2020-05-14 01:41:04 +0900

    [BE] fix: cors error
    - fix cors error

--- BE/src/main/java/com/codesquad/baseball06/config/WebMvcConfigurerImpl.java
+++ BE/src/main/java/com/codesquad/baseball06/config/WebMvcConfigurerImpl.java
@@ -2,32 +2,15 @@ package com.codesquad.baseball06.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebMvcConfigurerImpl implements WebMvcConfigurer {
-
-//  private final JwtService jwtService;
-//
-//  public WebMvcConfigurerImpl(JwtService jwtService) {
-//    this.jwtService = jwtService;
-//  }
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("*")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .allowedMethods("GET", "POST", "OPTION", "HEADER", "PUT", "DELETE");
+        .allowedMethods("*");
   }
-
-//  @Override
-//  public void addInterceptors(InterceptorRegistry registry) {
-//    registry.addInterceptor(new HttpInterceptor(jwtService))
-//        //      .addPathPatterns("/develop/**")
-//        .excludePathPatterns("/**");
-//  }
 }
