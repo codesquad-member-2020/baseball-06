403784c47efc91947a625c05c54887f01b1cefba
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-15 02:26:09 +0900

    [BE] feat: add method for getAllTeamInfo for InningService and teamDao
    #59

--- BE/src/main/java/com/codesquad/baseball06/model/dao/TeamDao.java
+++ BE/src/main/java/com/codesquad/baseball06/model/dao/TeamDao.java
@@ -2,10 +2,12 @@ package com.codesquad.baseball06.model.dao;
 
 import static com.codesquad.baseball06.model.query.TeamQuery.FIND_BY_ID;
 
-import com.codesquad.baseball06.model.dao.mapper.unused.TeamMapper;
+import com.codesquad.baseball06.model.dao.mapper.EachTeamMapper;
+import com.codesquad.baseball06.model.dao.mapper.TeamMapper;
 import com.codesquad.baseball06.model.entity.Team;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.codesquad.baseball06.model.query.TeamQuery;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -16,16 +18,37 @@ public class TeamDao {
 
   private final NamedParameterJdbcTemplate jdbcTemplate;
   private final TeamMapper teamMapper;
+  private final EachTeamMapper eachTeamMapper;
 
-  public TeamDao(NamedParameterJdbcTemplate jdbcTemplate, TeamMapper teamMapper) {
+  public TeamDao(NamedParameterJdbcTemplate jdbcTemplate, TeamMapper teamMapper,
+      EachTeamMapper eachTeamMapper) {
     this.jdbcTemplate = jdbcTemplate;
     this.teamMapper = teamMapper;
+    this.eachTeamMapper = eachTeamMapper;
   }
 
   public Team findTeamById(Long id) {
     SqlParameterSource namedParameters = new MapSqlParameterSource()
         .addValue("id", id);
 
-    return jdbcTemplate.queryForObject(FIND_BY_ID, namedParameters, teamMapper).get(0);
+    return jdbcTemplate.queryForObject(TeamQuery.FIND_BY_ID, namedParameters, eachTeamMapper);
+  }
+
+  public List<Team> getAllTeamInfoByGameId(Long game_id) {
+    SqlParameterSource namedParameters = new MapSqlParameterSource()
+        .addValue("game_id", game_id);
+
+    List<Team> returnTeams = new ArrayList<>();
+
+    Team awayTeam = jdbcTemplate
+        .queryForObject(TeamQuery.FIND_AWAY_TEAM_BY_GAME_ID, namedParameters, eachTeamMapper);
+
+    Team homeTeam = jdbcTemplate
+        .queryForObject(TeamQuery.FIND_HOME_TEAM_BY_GAME_ID, namedParameters, eachTeamMapper);
+
+    returnTeams.add(awayTeam);
+    returnTeams.add(homeTeam);
+
+    return returnTeams;
   }
 }
