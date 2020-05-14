1f2f29e17c6204d5e2377ae08621bb762fe27641
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-05-15 03:29:20 +0900

    [BE] chore: add more team and player in data.sql

--- BE/src/main/resources/data.sql
+++ BE/src/main/resources/data.sql
@@ -1,82 +1,88 @@
 INSERT INTO team (name)
-VALUES ('BE HONUX');
+VALUES ('백엔드');
 INSERT INTO team (name)
-VALUES ('DOOSAN d home');
+VALUES ('후론트');
 INSERT INTO team (name)
-VALUES ('FE CRONG');
+VALUES ('iOS');
 INSERT INTO team (name)
-VALUES ('SAMSUNG l home');
-INSERT INTO team (name)
-VALUES ('SKT away');
-INSERT INTO team (name)
-VALUES ('GIANT home');
-
+VALUES ('마스터');
 
 INSERT INTO player (team_id, type, name)
-VALUES (1, 0, '투수HONUX');
+VALUES (1, 0, '투수 Dan');
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (1, 1, '타자1DION', 0.333);
+VALUES (1, 1, '타자1 Dion', 0.393);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (1, 1, '타자2JINIE', 0.333);
+VALUES (1, 1, '타자2 David', 0.365);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (1, 1, '타자3CORY', 0.333);
+VALUES (1, 1, '타자3 XP', 0.337);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (1, 1, '타자4 Solar', 0.389);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (1, 1, '타자5 해피밀', 0.328);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (1, 1, '타자6 Sunny', 0.328);
 
 INSERT INTO player (team_id, type, name)
-VALUES (2, 0, '투수JK');
+VALUES (2, 0, '투수 won');
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (2, 1, '타자1DAVID', 0.333);
+VALUES (2, 1, '타자1 Ellin', 0.384);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (2, 1, '타자2DELMA', 0.333);
+VALUES (2, 1, '타자2 hoo', 0.397);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (2, 1, '타자3XP', 0.333);
+VALUES (2, 1, '타자3 baekCo', 0.378);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (2, 1, '타자4ALEX', 0.333);
+VALUES (2, 1, '타자4 Taek', 0.356);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (2, 1, '타자5HAMIL', 0.333);
+VALUES (2, 1, '타자5 hoi', 0.345);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (2, 1, '타자6 Huey', 0.345);
 
 INSERT INTO player (team_id, type, name)
-VALUES (3, 0, '투수CRONG');
+VALUES (3, 0, '투수 delma');
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (3, 1, '타자1SIGRID', 0.333);
+VALUES (3, 1, '타자1 또치', 0.393);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (3, 1, '타자2WON', 0.333);
+VALUES (3, 1, '타자2 Olaf', 0.316);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (3, 1, '타자3SALLY', 0.333);
+VALUES (3, 1, '타자3 하이디', 0.377);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (3, 1, '타자4 Cory', 0.398);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (3, 1, '타자5 GangWoon', 0.323);
+INSERT INTO player (team_id, type, name, batting_average)
+VALUES (3, 1, '타자6 jinie', 0.323);
 
 INSERT INTO player (team_id, type, name)
-VALUES (4, 0, '투수HENRY');
+VALUES (4, 0, '투수 JK');
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (4, 1, '타자1DAN', 0.333);
+VALUES (4, 1, '타자1 Honux', 0.499);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (4, 1, '타자2OLAF', 0.333);
+VALUES (4, 1, '타자2 Crong', 0.373);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (4, 1, '타자3SOLAR', 0.333);
-
-INSERT INTO player (team_id, type, name)
-VALUES (5, 0, '투수SARAH');
+VALUES (4, 1, '타자3 Yagom', 0.393);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (5, 1, '타자1HOI', 0.333);
+VALUES (4, 1, '타자4 Head', 0.353);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (5, 1, '타자2DOOCHI', 0.333);
+VALUES (4, 1, '타자5 Sarah', 0.383);
 INSERT INTO player (team_id, type, name, batting_average)
-VALUES (5, 1, '타자3ELLIE', 0.333);
-
-INSERT INTO player (team_id, type, name)
-VALUES (6, 0, '투수GIANT');
-INSERT INTO player (team_id, type, name, batting_average)
-VALUES (6, 1, '타자1HAN', 0.333);
-INSERT INTO player (team_id, type, name, batting_average)
-VALUES (6, 1, '타자2POOGLE', 0.333);
-INSERT INTO player (team_id, type, name, batting_average)
-VALUES (6, 1, '타자3DINGO', 0.333);
+VALUES (4, 1, '타자6 javajigi', 0.373);
 
 INSERT INTO game (away_id, home_id, away_user, home_user)
 VALUES (1, 2, 'dan@gmail.com', 'sigrid@naver.com');
 
 INSERT INTO game (away_id, home_id)
-VALUES (3, 4);
+VALUES (1, 3);
 
 INSERT INTO game (away_id, home_id)
-VALUES (5, 6);
+VALUES (1, 4);
+
+INSERT INTO game (away_id, home_id)
+VALUES (2, 3);
+
+INSERT INTO game (away_id, home_id)
+VALUES (2, 4);
+
+INSERT INTO game (away_id, home_id)
+VALUES (3, 4);
 
 
